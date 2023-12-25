bccb2c24d6040de2251469cb12d94a821f8efb6c
Abhay Panchal <abpanchal951357@gmail.com>
2023-12-25 17:15:40 +0530

    Database connection done!

--- yt-backend/src/index.js
+++ yt-backend/src/index.js
@@ -1,1 +1,38 @@
-console.log("Running");
+// one way to write connect db function!
+/*
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+import express from "express";
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+    app.on("error", (error) => {
+      console.log("Error", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App listening on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.log("Error ", error);
+    throw error;
+  }
+})();
+*/
+
+// code consistency gets break :( bcoz we are not using require
+// require("dotenv").config({
+//   path: "./env",
+// });
+import connectDb from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDb();
